931964496efd66b41adc9ab050976b7705f5fe03
jeongEulhyeong <144295621+jeongEulhyeong@users.noreply.github.com>
2023-10-17 12:52:58 +0900

    Add files via upload
    23년 10월 17일 12시 52분에 코딩 수업 내용을 업로드

---
+++ jeh_stu.py
@@ -0,0 +1,12 @@
+# 10명의 성적을 입력  받아 평균을 내는 프로그램을 만든다.
+# 반복문과 조건문을 사용한다.
+# 변수는 total, counter, grade, average.
+total = 0           #변수 total은 0으로 선언
+counter = 1             #변수 counter는 1로 선언
+while counter <= 10 :       #반복문 while을 사용하여 counter가 10보다 크거나 같을 때 내부  문장으로 넘어간다.
+    grade = int(input("성적을 입력하시오."))            #변수 grade에 int(정수형 변환)와 사용자로부터 입력을 받는 input을 사용하여 점수를 받는다.
+    total = grade + total           #변수 total에 성적 grade와 total 합한 값을 선언
+    counter = counter + 1           #변수 counter는 초기 선언한 counter에 값에 실행할 때마다 1씩 추가한다.
+average = total / 10           #변수 average에 반복문에서 계산한 total 값을 10으로 나눈다.
+print(average)          #평균값을 출력
+# writh by 정을형
